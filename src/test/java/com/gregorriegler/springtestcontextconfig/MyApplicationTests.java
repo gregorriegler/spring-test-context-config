8f8a8a5a2c8ed5bf5fcf5a70e3b6defaf74caa1b
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-24 21:30:29 +0100

    @SpringBootTest reads property from main application.properties

--- src/test/java/com/gregorriegler/springtestcontextconfig/MyApplicationTests.java
+++ src/test/java/com/gregorriegler/springtestcontextconfig/MyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.gregorriegler.springtestcontextconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MyApplicationTests {
 
+	@Value("${some.property}")
+	String someProperty;
+
+	@Test
+	void reads_property_from_main_application_properties() {
+		assertThat(someProperty).isEqualTo("default");
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
 }
