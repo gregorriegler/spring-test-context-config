4efd1f43eac5e4021a0fdbf69e74605087c54501
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-24 21:35:47 +0100

    @SpringBootTest overrides property from main application.properties with profile properties

--- src/test/java/com/gregorriegler/springtestcontextconfig/MyApplicationTests.java
+++ src/test/java/com/gregorriegler/springtestcontextconfig/MyApplicationTests.java
@@ -3,10 +3,12 @@ package com.gregorriegler.springtestcontextconfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class MyApplicationTests {
 
 	@Value("${some.property}")
@@ -15,6 +17,14 @@ class MyApplicationTests {
 	@Test
 	void reads_property_from_main_application_properties() {
 		assertThat(someProperty).isEqualTo("default");
+	}
+
+	@Value("${profile.overridden.property}")
+	String profileOverriddenProperty;
+
+	@Test
+	void overrides_properties_via_profile_application_properties() {
+		assertThat(profileOverriddenProperty).isEqualTo("overridden");
 	}
 
 	@Test
